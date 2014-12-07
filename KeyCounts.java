b34f6278cd47b436ead5b0b6a7cda06da547b3b7
sean-charboneau <seancharboneau@gmail.com>
2014-12-07 12:58:40 -0800

    adds files for dealer.com quiz

---
+++ KeyCounts.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class KeyCounts
+{
+	public static void main(String[] args)
+	{
+		outputKeyCounts("input.txt");
+		outputKeyCounts("input1.txt");
+		outputKeyCounts("input2.txt");
+	}
+	
+	public static void outputKeyCounts(String filename)
+	{
+		try
+		{
+			HashMap<String, Integer> counts = sumKeyCounts(filename);
+			Iterator<String> keySetIterator = counts.keySet().iterator();
+
+			// Iterate through the results and print them out nicely
+			while(keySetIterator.hasNext())
+			{
+				String key = keySetIterator.next();
+				System.out.println("The total for " + key + " is " + counts.get(key) + ".");
+			}
+			System.out.println("-----");
+		}
+		catch (IOException e)
+		{
+			// File not found
+			System.out.println("Error loading file");
+			e.printStackTrace();
+		}
+	}
+	
+	// PRECONDITIONS
+	// The file contains data in the format "key,count"
+	// where key is a string and count is an integer.
+	public static HashMap<String, Integer> sumKeyCounts(String filename) throws IOException
+	{
+		HashMap<String, Integer> counts = new HashMap<String, Integer>();
+		
+		// Load the file from the local directory
+		BufferedReader br = new BufferedReader(new InputStreamReader(KeyCounts.class.getClassLoader().getResourceAsStream(filename)));
+	    try
+	    {
+	        String line = br.readLine();
+
+	        // For each line, either add to the existing entry or create a new one
+	        while (line != null)
+	        {
+	        	String[] parts = line.split(",");
+	        	String name = parts[0];
+	        	int sum = Integer.parseInt(parts[1]);
+	        	
+	        	if (counts.containsKey(name))
+	        	{
+	        		sum += counts.get(name);
+	        	}
+	        	
+	        	counts.put(name, sum);
+	            line = br.readLine();
+	        }
+	    }
+	    catch (IOException e)
+	    {
+	    	// The file contained an invalid entry
+			System.out.println("Error reading file contents");
+			e.printStackTrace();
+		}
+	    finally
+	    {
+	        br.close();
+	    }
+		
+		return counts;
+	}
+}
---
+++ Palindrome.java
@@ -0,0 +1,34 @@
+public class Palindrome
+{
+	public static void main(String[] args)
+	{
+		testPalindrome(null);
+		testPalindrome("");
+		testPalindrome("testing");
+		testPalindrome("race car$#%^");
+		testPalindrome("A car, a man, a maraca.");
+		testPalindrome("Slap a ham on Omaha, pals.");
+		testPalindrome("Space cats");
+	}
+
+	// We'll separate this from the display method for modularity
+	public static boolean isPalindrome(String input)
+	{
+		// Wasn't specified how to handle these cases, but we'll consider them non-palindromes here
+		if (input == null || input.length() == 0)
+		{
+			return false;
+		}
+		
+		// We're ignoring any characters that are not alphanumeric
+		input = input.replaceAll("[^A-Za-z0-9]", "");
+		// Just reverse the string and compare it to the original
+		return input.equalsIgnoreCase(new StringBuffer(input).reverse().toString());
+	}
+	
+	public static void testPalindrome(String input)
+	{
+		System.out.print("\"" + input + "\" IS ");
+		System.out.println(isPalindrome(input) ? "a palindrome." : "NOT a palindrome");
+	}
+}
