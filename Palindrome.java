3dee7f57e45f8b2595a05362dbb3c3f8279ec6c0
sean-charboneau <seancharboneau@gmail.com>
2014-12-07 13:12:40 -0800

    tweaks the code styling

--- Palindrome.java
+++ Palindrome.java
@@ -14,21 +14,24 @@ public class Palindrome
 	// We'll separate this from the display method for modularity
 	public static boolean isPalindrome(String input)
 	{
-		// Wasn't specified how to handle these cases, but we'll consider them non-palindromes here
+		// Wasn't specified how to handle these cases, but we'll consider them
+		// non-palindromes here
 		if (input == null || input.length() == 0)
 		{
 			return false;
 		}
-		
+
 		// We're ignoring any characters that are not alphanumeric
 		input = input.replaceAll("[^A-Za-z0-9]", "");
 		// Just reverse the string and compare it to the original
-		return input.equalsIgnoreCase(new StringBuffer(input).reverse().toString());
+		return input.equalsIgnoreCase(new StringBuffer(input).reverse()
+		        .toString());
 	}
-	
+
 	public static void testPalindrome(String input)
 	{
 		System.out.print("\"" + input + "\" IS ");
-		System.out.println(isPalindrome(input) ? "a palindrome." : "NOT a palindrome");
+		System.out.println(isPalindrome(input) ? "a palindrome."
+		        : "NOT a palindrome");
 	}
 }
